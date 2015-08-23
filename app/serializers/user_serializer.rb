3a8738e6f7652a2db033f029d50937cfd0b001f1
Thadeu Brito <itvbrito@gmail.com>
2015-08-23 01:46:44 -0300

    Added user attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :access_token
+  attributes :id, :email, :access_token, :first_name, :last_name, :name
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,11 @@ ActiveRecord::Schema.define(version: 20150822184116) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",              null: false
+    t.string   "first_name",         null: false
+    t.string   "last_name",          null: false
     t.string   "encrypted_password", null: false
     t.string   "access_token",       null: false
+    t.boolean  "admin"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
